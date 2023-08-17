5088eac03617d995d94a082938de3082c8ee7ad2
Luis Henrique Rocha <henrique.srocha@gmail.com>
2023-08-18 00:00:17 +0200

    feat: 12. Clamping The Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,11 +7,16 @@ public class CameraController : MonoBehaviour
 
     private PlayerController player;
     private Vector3 cameraOffset = new Vector3(0f, 0f, -10f);
+    [SerializeField] private BoxCollider2D boundsBox;
+    private float halfHeight, halfWidth;
 
     // Start is called before the first frame update
     void Start()
     {
         this.player = FindObjectOfType<PlayerController>();
+
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
     }
 
     // Update is called once per frame
@@ -20,7 +25,10 @@ public class CameraController : MonoBehaviour
         if (player != null)
         {
             // camera follow player
-            this.transform.position = this.player.transform.position + cameraOffset;
+            Vector3 desiredCameraPosition = this.player.transform.position + cameraOffset;
+            desiredCameraPosition.x = Mathf.Clamp(desiredCameraPosition.x, boundsBox.bounds.min.x + halfWidth, boundsBox.bounds.max.x - halfWidth);
+            desiredCameraPosition.y = Mathf.Clamp(desiredCameraPosition.y, boundsBox.bounds.min.y + halfHeight, boundsBox.bounds.max.y - halfHeight);
+            this.transform.position = desiredCameraPosition;
         }
     }
 }
