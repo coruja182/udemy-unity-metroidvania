0861d1d5866cd0a6a96c7813f1774543f3aec29f
Luis Henrique Rocha <henrique.srocha@gmail.com>
2023-08-18 00:32:32 +0200

    feat: 13. Creating A Bullet

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private Rigidbody2D bulletRigidBody2D;
+    [SerializeField] private Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move the bullet
+        bulletRigidBody2D.velocity = moveDirection * bulletSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
--- README.md
+++ README.md
@@ -11,5 +11,5 @@ Window --> Layouts -> 2 by 3
 
 Window --> 2D --> Tile Palette --> *drag beside Inspector panel*
 
-Edit --> Project Settings... --> Editor --> *check* Enter Play Mode Options
+Edit --> Project Settings... --> Editor --> *check* Enter Play Mode Options, reload domain, reload scene
 ```
