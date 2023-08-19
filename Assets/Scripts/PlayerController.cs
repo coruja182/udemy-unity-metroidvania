ed15735d9dff8bcf1fae9d55fd5b8540b2b6cd67
Luis Henrique Rocha <henrique.srocha@gmail.com>
2023-08-19 20:10:09 +0200

    feat: 19. Dashing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,10 +15,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Animator anim;
     [SerializeField] private BulletController shotToFire;
     [SerializeField] private Transform shotPoint;
+    [SerializeField] private float m_dashSpeed, m_dashTime;
 
     private bool facingRight = true;
     private bool isOnGround;
     private bool canDoubleJump;
+    private float dashCounter;
 
     // Start is called before the first frame update
     void Start()
@@ -29,19 +31,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // move sideways
-        // the teacher prefers GetAxisRaw instead of GetAxis for platforming 2D games
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        characterRigidbody.velocity = new Vector2(moveInput * moveSpeed, characterRigidbody.velocity.y); ;
-
-        // Flip the character's velocity for movement
-        if (moveInput > 0 && !facingRight || moveInput < 0 && facingRight)
-        {
-            FlipCharacter();
-        }
-
         // whether we the player is touching the ground
         isOnGround = Physics2D.OverlapCircle(groundPoint.position, .2f, whatIsGround);
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            dashCounter = m_dashTime;
+        }
+
+        if (dashCounter > 0)
+        {
+            // Time.deltaTime = the interval in seconds from the last frame to this current one
+            dashCounter -= Time.deltaTime;
+            characterRigidbody.velocity = new Vector2(m_dashSpeed * transform.localScale.x, characterRigidbody.velocity.y);
+        }
+        else
+        {
+            // move sideways
+            // the teacher prefers GetAxisRaw instead of GetAxis for platforming 2D games
+            float moveInput = Input.GetAxisRaw("Horizontal");
+            characterRigidbody.velocity = new Vector2(moveInput * moveSpeed, characterRigidbody.velocity.y); ;
+
+            // Flip the character's velocity for movement
+            if (moveInput > 0 && !facingRight || moveInput < 0 && facingRight)
+            {
+                FlipCharacter();
+            }
+        }
 
         // Jumping and Double Jump
         if (Input.GetButtonDown("Jump") && (isOnGround || canDoubleJump))
@@ -64,6 +80,7 @@ public class PlayerController : MonoBehaviour
 
         anim.SetBool("isOnGround", isOnGround);
         anim.SetFloat("speed", Mathf.Abs(characterRigidbody.velocity.x));
+
     }
 
     private void FlipCharacter()
