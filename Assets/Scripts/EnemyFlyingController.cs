f21b234ebd890c1ffae8dffd23157bf7630bce47
Luis Henrique Silveira da Rocha <henrique.srocha@gmail.com>
2023-08-26 21:09:37 +0200

    feat: 35. Setting Up a Flying Enemy

---
+++ Assets/Scripts/EnemyFlyingController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFlyingController : MonoBehaviour
+{
+
+    [SerializeField] private float m_rangeToStartChase, m_moveSpeed, m_turnSpeed;
+    private bool m_isChasing;
+    private Transform m_playerRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_playerRef = PlayerHealthController.Instance.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!m_isChasing)
+        {
+            // should the enemy chase?
+            if (Vector3.Distance(transform.position, m_playerRef.position) < m_rangeToStartChase)
+            {
+                m_isChasing = true;
+            }
+        }
+        else
+        {
+            LookTorwardsPlayer();
+        }
+    }
+
+    private void LookTorwardsPlayer()
+    {
+        // chase player
+        Vector3 directionToPlayer = transform.position - m_playerRef.position;
+        Debug.Log("Direction to player: " + directionToPlayer);
+        float angleToPlayer = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.AngleAxis(angleToPlayer, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, m_turnSpeed * Time.deltaTime);
+    }
+}
