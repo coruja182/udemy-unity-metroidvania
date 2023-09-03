9d70c94177b2563adfecef0c3f457ee0f4a5ebec
Luis Henrique Rocha <henrique.srocha@gmail.com>
2023-09-03 17:55:59 +0200

    feat: 56. The First Phase

--- Assets/Scripts/BossBattle.cs
+++ Assets/Scripts/BossBattle.cs
@@ -5,21 +5,68 @@ using UnityEngine;
 public class BossBattle : MonoBehaviour
 {
     [SerializeField] private Transform m_cameraPosition;
-    [SerializeField] private float m_cameraSpeed;
+    [SerializeField] private float m_cameraSpeed, m_activeTime, m_fadeOutTime, m_inactiveTime, m_moveSpeed;
+    [SerializeField] private int m_threshold_1, m_threshold_2;
+    [SerializeField] private Transform[] m_spawnPoints;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Transform m_bossObject;
 
     private CameraController m_cameraController;
+    private float m_activeCounter, m_fadeCounter, m_inactiveCounter;
+
+    private Transform m_targetPoint;
 
     // Start is called before the first frame update
     void Start()
     {
         m_cameraController = FindObjectOfType<CameraController>();
         m_cameraController.enabled = false;
+
+        m_activeCounter = m_activeTime;
     }
 
     // Update is called once per frame
     void Update()
     {
         m_cameraController.transform.position = Vector3.MoveTowards(m_cameraController.transform.position, m_cameraPosition.position, m_cameraSpeed * Time.deltaTime);
+
+        // Boss Phase 1
+        if (BossHealthController.Instance.CurrentHealth > m_threshold_1)
+        {
+            if (m_activeCounter > 0f)
+            {
+                m_activeCounter -= Time.deltaTime;
+                if (m_activeCounter <= 0f)
+                {
+                    m_fadeCounter = m_fadeOutTime;
+                    animator.SetTrigger("vanish");
+                }
+            }
+            else if (m_fadeCounter > 0)
+            {
+                m_fadeCounter -= Time.deltaTime;
+                if (m_fadeCounter <= 0f)
+                {
+                    m_bossObject.gameObject.SetActive(false);
+                    m_inactiveCounter = m_inactiveTime;
+                }
+            }
+            else if (m_inactiveCounter > 0)
+            {
+                m_inactiveCounter -= Time.deltaTime;
+                if (m_inactiveCounter <= 0f)
+                {
+                    // boss reappears
+                    m_bossObject.position = m_spawnPoints[Random.Range(0, m_spawnPoints.Length)].position;
+
+                    // activates the boss
+                    m_bossObject.gameObject.SetActive(true);
+
+                    // resets the active acounter
+                    m_activeCounter = m_activeTime;
+                }
+            }
+        }
     }
 
     public void EndBattle()
--- Assets/Scripts/BossHealthController.cs
+++ Assets/Scripts/BossHealthController.cs
@@ -6,10 +6,11 @@ using UnityEngine.UI;
 public class BossHealthController : MonoBehaviour
 {
     public static BossHealthController Instance { get; private set; }
-
     [SerializeField] Slider m_bossHealthSlider;
     [SerializeField] private int m_currentHealth = 30;
     [SerializeField] private BossBattle m_bossBattle;
+
+    public int CurrentHealth { get => m_currentHealth; set => m_currentHealth = value; }
 
     private void Awake()
     {
@@ -26,19 +27,19 @@ public class BossHealthController : MonoBehaviour
 
     private void Start()
     {
-        m_bossHealthSlider.maxValue = m_currentHealth;
-        m_bossHealthSlider.value = m_currentHealth;
+        m_bossHealthSlider.maxValue = CurrentHealth;
+        m_bossHealthSlider.value = CurrentHealth;
     }
 
     public void TakeDamage(int damageAmount)
     {
-        m_currentHealth = Mathf.Max(m_currentHealth - damageAmount, 0);
+        CurrentHealth = Mathf.Max(CurrentHealth - damageAmount, 0);
 
-        if (m_currentHealth == 0)
+        if (CurrentHealth == 0)
         {
             m_bossBattle.EndBattle();
         }
 
-        m_bossHealthSlider.value = m_currentHealth;
+        m_bossHealthSlider.value = CurrentHealth;
     }
 }
