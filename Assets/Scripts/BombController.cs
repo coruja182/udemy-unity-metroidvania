0b53bb4a14644eba53d3d8e7e25db3fe7624b3db
Luis Henrique Rocha <henrique.srocha@gmail.com>
2023-09-12 22:36:47 +0200

    feat: 64. Damaging Enemies With Bombs

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -9,6 +9,8 @@ public class BombController : MonoBehaviour
     [SerializeField] private GameObject m_explosionGameObject;
     [SerializeField] private float m_blastRange;
     [SerializeField] private LayerMask m_whatIsDestructible;
+    [SerializeField] private LayerMask m_whatIsDamageable;
+    [SerializeField] private int m_damageAmount;
 
     // Start is called before the first frame update
     void Start()
@@ -34,6 +36,16 @@ public class BombController : MonoBehaviour
                 foreach (Collider2D objectToDamage in objectsToRemove)
                 {
                     Destroy(objectToDamage.gameObject);
+                }
+            }
+
+            Collider2D[] objetctsToDamage = Physics2D.OverlapCircleAll(transform.position, m_blastRange, m_whatIsDamageable);
+            if (objetctsToDamage.Length > 0)
+            {
+                foreach(Collider2D objectsToDamage in objetctsToDamage)
+                {
+                    EnemyHealthController enemyHealth = objectsToDamage.GetComponent<EnemyHealthController>();
+                    enemyHealth?.DamageEnemy(m_damageAmount);
                 }
             }
         }
